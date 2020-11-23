fe49e7d13699a60c5e79b7898086c822470d8ca4
Rodrigo Braga <rbraga@gmail.com>
2020-11-23 16:45:00 -0300

    Update admin.py
    Enable Admin to Sales

--- api/sales/admin.py
+++ api/sales/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sale
+
+
+class SaleAdmin(admin.ModelAdmin):
+   list_display = (
+      "code",
+      "value",
+      "date",
+      "reseller",
+      "status",
+      "percentage",
+      "cashback"
+   )
+   list_filter = ("reseller", "status", "date",)
+
+
+admin.site.register(Sale, SaleAdmin)
