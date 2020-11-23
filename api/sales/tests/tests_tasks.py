41ff62ad34f99f8d89ce5a2ecd886231261e7e34
Rodrigo Braga <rbraga@gmail.com>
2020-11-23 19:47:39 -0300

    Update tests_tasks.py
    Fix tests to tasks to avoid model.create

--- api/sales/tests/tests_tasks.py
+++ api/sales/tests/tests_tasks.py
@@ -1,27 +1,30 @@
 from unittest import mock
 
-from django.test import TestCase, override_settings
-
-from model_bakery import baker
+from django.test import TestCase
 
 from sales.tasks import automatic_approve, process
 
 
 class AutomaticApproveTest(TestCase):
+    @mock.patch("sales.models.Sale.objects.get")
     @mock.patch("sales.models.Sale.automatic_approve")
-    def test_automatic_approve(self, m_automatic_approve):
-        reseller = baker.make_recipe("users.user")
-        sale = baker.make_recipe("sales.sale", reseller=reseller)
+    def test_automatic_approve(self, m_automatic_approve, m_get):
+        sale = mock.Mock(id=1)
+
         automatic_approve(sale_id=sale.id)
 
-        m_automatic_approve.assert_called_once
+        sale.assert_called_once
 
 
 class ProcessTest(TestCase):
+    @mock.patch("sales.models.Sale.objects.get")
     @mock.patch("sales.models.Sale.process")
-    def test_process(self, m_process):
-        sale = baker.make_recipe("sales.sale", value=2000)
+    def test_process(self, m_process, m_get):
+        sale = mock.Mock(id=1)
+
+        m_get.return_value = sale
+
         process(sale_id=sale.id)
-        
-        m_process.assert_called_once()
+
+        sale.process.assert_called_once()
 
