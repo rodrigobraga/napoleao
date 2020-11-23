23b6847d9e4ad4f90c684d0b54950ee2be9881c5
Rodrigo Braga <rbraga@gmail.com>
2020-11-23 18:30:26 -0300

    Update urls.py
    Enable Swagger

--- api/api/urls.py
+++ api/api/urls.py
@@ -20,6 +20,23 @@ from rest_framework_simplejwt.views import TokenObtainPairView as obtain
 from rest_framework_simplejwt.views import TokenRefreshView as refresh
 from rest_framework_simplejwt.views import TokenVerifyView as verify
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Napoleão",
+      default_version='v1',
+      description="cashback calculator",
+      terms_of_service="https://policies.google.com/terms",
+      contact=openapi.Contact(email="rbraga@local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +47,15 @@ urlpatterns = [
 
     path("users/", include("users.urls", namespace="users")),
     path("sales/", include("sales.urls", namespace="sales")),
+
+    path(
+        'swagger(?P<format>\.json|\.yaml)',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
 ]
